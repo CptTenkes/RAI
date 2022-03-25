2e04909b004b6adb3ff3cb6e1389968a274c9617
Salvador Mandujano Rodríguez <32318328+SMandujanoR@users.noreply.github.com>
2022-03-25 10:19:11 -0600

    Add files via upload

---
+++ R/RAImapSp.R
@@ -0,0 +1,30 @@
+#' Function to graph the RAI of selected species
+#'
+#' @author SMandujanoR
+#' @param mapa Introduce map
+#' @param species Select the species
+#' @param pointSize X
+#' @param mycolors Select
+#' @importFrom prettymapr addscalebar addnortharrow
+#' @importFrom utils read.csv
+#' @export
+
+
+RAImapSp <- function(mapa, species, pointSize, mycolors) {
+
+  new.mat <- read.csv("Results/Table_RAIalt.csv", header = T)
+  sp  <- subset(new.mat, Species == species)
+
+  jpeg(filename= paste("Results/Map_", str_to_title(species), ".jpg", sep = ""), width= 7000, height= 5000, units= "px", res=1200)
+
+  par(mfrow = c(1,1), mar = c(1,1,1,1))
+
+  plot(mapa, col = mycolors, fill = T, lty = 0, main = unique(sp$Species))
+  addscalebar(htin = 0.05, padin = c(0.05, 0.05), pos = "bottomright")
+  addnortharrow()
+
+  points(sp$X, sp$Y, pch = 16, col = "#00000170", cex = sp$RAIalt*pointSize)
+  text(sp$X, sp$Y, sp$RAIalt, cex = 0.3)
+
+  dev.off()
+}
